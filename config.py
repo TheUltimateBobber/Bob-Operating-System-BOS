0cfb892ec1f2cb7f77d637d1b17a15e712c3d5f1
TheUltimateBobber <134659603+TheUltimateBobber@users.noreply.github.com>
2023-05-27 21:58:10 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
-import time
-print('please download the fix-main branch version, or use one of the releases.')
-time.sleep(99999999)
+response = input('It is highly recommended that you use the fix-main branch or the latest release! (type "n" to abort')
+if(reponse == 'n' or response == 'N'):
+  import time
+  print('please download the fix-main branch version, or use one of the releases.')
+  time.sleep(99999999)
+import pc
+def StartSystem():
+	pc.__bootmodeset__('norm')
+	pc.testbootmode()
+	pc.__safemodeset__(True)
+	pc.testsafemode()
+	pc.start()
+StartSystem()
